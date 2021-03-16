7a1d85543a65095631291d805c5b02d8ae28e148
nguyenvt <nguyenvt@sheridancollege.ca>
2021-03-15 22:13:53 -0400

    add listing detail page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace CapstoneProject.Controllers
             return View();
         }
 
+        public IActionResult ListingDetail()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
