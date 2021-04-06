f3ae2a99895fa3244f2e791185d852a960096e57
jozwikc <jozwikc@sheridancollege.ca>
2021-04-05 23:15:51 -0400

    Added ISocialMedia interface

---
+++ Models/ISocialMedia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.Models
+{
+    public interface ISocialMedia
+    {
+        public Task createPost(string message, byte[] media);
+    }
+}
