0dc803d147c5853a17ae4a88978c4b58e08b16e3
viettuanncool <60185465+viettuanncool@users.noreply.github.com>
2021-09-21 10:45:55 -0400

    added apicontroller

---
+++ Models/PasswordAPIController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordAPIController : ControllerBase
+    {
+        SystemLoginCredential cred = new SystemLoginCredential();
+
+
+        [HttpPost]
+        public void Login(loginInfo input)
+        {
+            
+        }
+        [Route("api/[controller]/password/{pass}")]
+        [HttpGet]
+        public void ChangePassword(string password)
+        {
+
+        }
+
+        [Route("api/[controller]/username/{user}")]
+        [HttpGet]
+        public void ChangeUsername(string username)
+        {
+
+        }
+    }
+
+    public class loginInfo
+    {
+        string username;
+        string password;
+    }
+
+    public class SystemLoginCredential
+    {
+        protected string username = "admin";
+        protected string password = "12345678";
+
+        public bool CheckCredential(string user, string pass)
+        {
+            if(user.Equals(username) && pass.Equals(password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void ChangePassword(string pass)
+        {
+            bool check = false;
+            for(int i =0; i<pass.Count(); i++)
+            {
+                if (Char.IsUpper(pass[i]))
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (pass.Contains(" "))
+                check = false;
+            if(pass.Count() > 8 && pass!=password && check)
+            {
+                password = pass;
+            }
+        }
+
+        public void ChangeUsername(string user)
+        {
+            username = user;
+        }
+    }
+}
